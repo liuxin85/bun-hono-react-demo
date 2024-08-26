608109438d0b0572e58f05866b4e4019a1d8ee48
liuxin85 <liuxin85@gmail.com>
2024-08-26 23:14:44 +0800

    build: with hono rpc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,15 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { useEffect, useState } from 'react';
+import { api } from '@/lib/api';
 
 function App() {
   const [totalSpend, setTotalSepnd] = useState(0);
 
   useEffect(() => {
     async function fetchTotal() {
-      const res = await fetch('/api/expenses/total-spent');
+      // const res = await fetch('/api/expenses/total-spent');
+      const res = await api.expenses['total-spent'].$get();
       const data = await res.json();
 
       setTotalSepnd(data.total);
